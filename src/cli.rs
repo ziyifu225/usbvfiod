a84557bac4f114be5a9640ccf25fee2de128baa4
Julian Stecklina <julian.stecklina@cyberus-technology.de>
2025-04-08 18:02:25 +0200

    app: add the socket path the cli parameters

--- src/cli.rs
+++ src/cli.rs
@@ -1,3 +1,10 @@
+//! This module implements the CLI interface.
+//!
+//! The main external constraint here is that we need to be compatible
+//! to the vfio-user [Backend Program
+//! Conventions](https://github.com/nutanix/libvfio-user/blob/master/docs/vfio-user.rst#backend-program-conventions).
+use std::path::PathBuf;
+
 use clap::Parser;
 
 #[derive(Parser, Debug)]
@@ -13,4 +20,10 @@ pub struct Cli {
     /// increase verbosity.
     #[arg(short, long, action = clap::ArgAction::Count)]
     pub verbose: u8,
+
+    /// The path where to create a listening Unix domain socket.
+    ///
+    /// This is the path where Cloud Hypervisor will connect to usbvfiod.
+    #[arg(short, long)]
+    pub socket_path: PathBuf,
 }
