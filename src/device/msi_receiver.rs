98a5188d80d57368b364be6903c89e643eada29c
Julian Stecklina <julian.stecklina@cyberus-technology.de>
2025-05-28 16:09:17 +0200

    device: remove unused MSI receiver
    This is not going to be useful in this project, because whatever
    receives interrupt messages is not in the usbvfiod process.

--- src/device/msi_receiver.rs
+++ src/device/msi_receiver.rs
@@ -1,8 +1,4 @@
-//! # MSI Receiver
-//!
-//! This module contains a trait [`MsiReceiver`] which allows receiving Message-Signaled Interrupts
-//! (MSIs) with custom Address and Data type. Objects that implement this trait are supposed to be
-//! used by virtual devices that send MSIs.
+//! # MSI Messages
 
 use std::fmt::Debug;
 
@@ -30,51 +26,5 @@ impl MsiMessage {
     #[must_use]
     pub fn new(address: u64, data: u16) -> Self {
         Self { address, data }
-    }
-}
-
-/// Trait for all receivers of [`MsiMessage`]s.
-///
-/// It provides a single function for sending MSIs with a 64-bit address field and a 16-bit data
-/// field.
-///
-/// Devices using this trait to send interrupts are supposed to be implemented like this:
-///
-/// ```
-/// use usbvfiod::device::msi_receiver::{MsiReceiver, MsiMessage};
-///
-/// struct Device {
-///     vector: MsiMessage,
-///     msi_receiver: std::rc::Rc<dyn MsiReceiver>,
-/// }
-///
-/// impl Device {
-///     fn send_interrupt(&self) {
-///         self.msi_receiver.send_msi(self.vector);
-///     }
-/// }
-///```
-pub trait MsiReceiver: Debug + Send + Sync {
-    /// Sends a single MSI to the receiver.
-    fn send_msi(&self, msi: MsiMessage);
-}
-
-/// A do-nothing implementation of [`MsiReceiver`] useful for testing or prototyping.
-///
-/// Any MSIs sent to this receiver are silently dropped.
-#[derive(Debug, Clone, Copy, Default)]
-pub struct DummyMsiReceiver {}
-
-impl DummyMsiReceiver {
-    /// Create a new dummy MSI receiver.
-    #[must_use]
-    pub fn new() -> Self {
-        Self::default()
-    }
-}
-
-impl MsiReceiver for DummyMsiReceiver {
-    fn send_msi(&self, _msi: MsiMessage) {
-        // The dummy receiver intentionally does nothing when it receives an MSI.
     }
 }
