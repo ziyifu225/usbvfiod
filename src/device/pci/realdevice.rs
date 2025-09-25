3e32c30374b671c744663d03f155821cc874cf4c
Lukas Beierlieb <lukas.beierlieb@cyberus-technology.de>
2025-09-25 17:47:58 +0200

    realdevice: pass endpoint type to enable_endpoint
    The endpoint-type information is relevant for enabling the correct
    endpoints in the real device. This commit makes the endpoint type
    available in the enable_endpoint function, but does not make use of it
    yet (the enable_endpoint function will receive great rework to make the
    existing bulk endpoint handling asynchronous, making changes to handle
    interrupt endpoints does not make sense yet).

--- src/device/pci/realdevice.rs
+++ src/device/pci/realdevice.rs
@@ -38,7 +38,7 @@ impl fmt::Display for Speed {
 pub trait RealDevice: Debug {
     fn speed(&self) -> Option<Speed>;
     fn control_transfer(&self, request: &UsbRequest, dma_bus: &BusDeviceRef);
-    fn enable_endpoint(&mut self, endpoint_id: u8);
+    fn enable_endpoint(&mut self, endpoint_id: u8, endpoint_type: EndpointType);
     fn transfer_out(
         &mut self,
         endpoint_id: u8,
